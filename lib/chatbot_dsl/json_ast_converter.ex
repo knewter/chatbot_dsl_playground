ad8e05407536a4cb484a8e8cbc2363ed2636e4a1
Josh Adams <josh@isotope11.com>
2015-09-26 08:17:33 -0500

    Add json ast types, sort of

--- lib/chatbot_dsl/json_ast_converter.ex
+++ lib/chatbot_dsl/json_ast_converter.ex
@@ -1,9 +1,18 @@
 defmodule ChatbotDSL.JSONASTConverter do
+  alias ChatbotDSL.Transformer
+
   @moduledoc """
   The JSONASTConverter takes in a JSON AST and returns our custom AST.
   It exists to allow us to get around the fact that JSON doesn't support
   tuples and atoms, essentially.
   """
+
+  @type primitive :: String.t ## derp
+  @type expression_type :: String.t ## derp
+  @type expression :: String.t | %{
+    type: expression_type | primitive,
+    arguments: list(expression)
+  }
 
   def convert(json) do
     json
@@ -11,6 +20,7 @@ defmodule ChatbotDSL.JSONASTConverter do
     |> do_convert
   end
 
+  @spec do_convert(expression) :: {Transformer.expression}
   def do_convert(%{"type" => "if", "arguments" => arguments}) do
     {:if, Enum.map(arguments, &do_convert/1)}
   end
